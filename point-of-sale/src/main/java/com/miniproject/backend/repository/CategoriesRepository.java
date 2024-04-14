573d18dce602d1dc9339074e150afb73ac831bc1
RivaAndraYuni <156744078+RivaAndraYuni@users.noreply.github.com>
2024-04-15 01:05:45 +0700

    Delete point-of-sale directory

--- point-of-sale/src/main/java/com/miniproject/backend/repository/CategoriesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.miniproject.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.miniproject.backend.model.Categories;
-
-public interface CategoriesRepository extends JpaRepository<Categories, Integer> {
-
-  
-}
